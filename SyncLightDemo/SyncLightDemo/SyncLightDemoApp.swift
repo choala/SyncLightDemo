88eddfd3a54094cd058e6a771f16a06b34b54662
Chae-young Park <pcy07111@gmail.com>
2023-08-09 14:08:56 +0900

    feat: Build a project

---
+++ SyncLightDemo/SyncLightDemo/SyncLightDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyncLightDemoApp.swift
+//  SyncLightDemo
+//
+//  Created by 박채영 on 2023/08/09.
+//
+
+import SwiftUI
+
+@main
+struct SyncLightDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
